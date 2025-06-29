9618dc8b6fe82ecaf957ac80a25ffeffacb67173
hourtt <bunhourtjesda@gmail.com>
2025-06-29 10:53:46 +0700

    How to merge to code instruction

--- demo-management-main/src/main/java/com/example/management/repository/BoardRepository.java
+++ demo-management-main/src/main/java/com/example/management/repository/BoardRepository.java
@@ -10,3 +10,12 @@ public interface BoardRepository extends JpaRepository<Board,Long> {
     //Change from Page to Slice
     Slice<Board> findAllByCreatedBy(String writer, PageRequest pageRequest);
 }
+
+
+//* To Merge the code*//
+
+// 1. Create the new branch (In other word, second branch)
+// 2. Commit or push the file into that second branch
+// 3. check on the Github (It'll show and tell you to compare & pull request)
+//* 4. First, pull request and then merge the code into the first branch (Master in my case)
+//* 5. Wait until it finished to pull request and merged
