2533d1b9ad3d430404b6e6b45a7b9d472371f8e6
hourtt <bunhourtjesda@gmail.com>
2025-06-29 10:39:38 +0700

    Importing Slice and Service package and add the @Service

--- demo-management-main/src/main/java/com/example/management/service/BoardService.java
+++ demo-management-main/src/main/java/com/example/management/service/BoardService.java
@@ -4,10 +4,13 @@ import com.example.management.domain.Board;
 import com.example.management.repository.BoardRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service  // missing this line
 public class BoardService {
     private final BoardRepository boardRepository;
 
@@ -26,12 +29,13 @@ public class BoardService {
     }
 
     //Paging type 2
-    public Page<Board> getBoardList2(PageRequest pageRequest) {
-        return boardRepository.findAll(pageRequest);
+    public Slice<Board> getBoardList2(PageRequest pageRequest) {
+        // return boardRepository.findAll(pageRequest);
+        //Replace from findAll to findAllByCreateBy
+        return boardRepository.findAllByCreatedBy("Writer", pageRequest);
     }
 
     //get board
-
 
     //add process
     public void saveBoard(Board board) {
@@ -39,14 +43,18 @@ public class BoardService {
     }
 
     public void bulkBoard() {
-        List<Board> list = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            Board board = new Board();
-            board.setCreatedBy("Writer");
-            board.setTitle("bulk title - " + 1);
-            board.setContent("bulk content - " + 1);
-            list.add(board);
+        try {
+            List<Board> list = new ArrayList<>();
+            for (int i = 0; i < 100; i++) {
+                Board board = new Board();
+                board.setCreatedBy("Writer");
+                board.setTitle("bulk title - " + 1);
+                board.setContent("bulk content - " + 1);
+                list.add(board);
+            }
+            boardRepository.saveAll(list);
+        } catch(Exception e) {
+            e.printStackTrace();
         }
-        boardRepository.saveAll(list);
     }
 }
