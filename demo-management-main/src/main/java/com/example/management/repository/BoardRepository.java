f2906181531a5f77d18b00f5fb1c1e1c3a2d15f1
hourtt <bunhourtjesda@gmail.com>
2025-06-29 10:45:09 +0700

    Importing the Page, PageRequest and Slice package and change from Page to Slice at     Slice<Board> findAllByCreatedBy(String writer, PageRequest pageRequest);

--- demo-management-main/src/main/java/com/example/management/repository/BoardRepository.java
+++ demo-management-main/src/main/java/com/example/management/repository/BoardRepository.java
@@ -1,7 +1,12 @@
 package com.example.management.repository;
 
 import com.example.management.domain.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
+    //Change from Page to Slice
+    Slice<Board> findAllByCreatedBy(String writer, PageRequest pageRequest);
 }
